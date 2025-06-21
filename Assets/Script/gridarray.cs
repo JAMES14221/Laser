7475474e7d863976f8b4aa7432f65a6642fbfdde
James <nguyenchian8@gmail.com>
2025-06-21 14:06:07 +0700

    Bulb check, scene change mech

--- Assets/Script/gridarray.cs
+++ Assets/Script/gridarray.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.SceneManagement;
+
 public class gridarray : MonoBehaviour
 {
     public static gridarray Instance; // singleton reference
@@ -22,5 +24,37 @@ public class gridarray : MonoBehaviour
                 grid[y, x] = "0"; 
             }
         }
+
+
+
+
+        bool BulbsActiveState()
+        {
+            GameObject[] tiles = GameObject.FindGameObjectsWithTag("bulb");
+
+            foreach (GameObject tile in tiles)
+            {
+                Vector3 pos = tile.transform.localPosition;
+                int x = Mathf.RoundToInt(pos.x / 0.75f);
+                int y = Mathf.RoundToInt(pos.y / 0.75f);
+
+                if (!gridarray.Instance.grid[y, x].Contains("5"))
+                {
+                    return false; // Found an inactive tile!
+                }
+            }
+
+            return true; // All tiles are active!
+        }
+
+        string currentScene = SceneManager.GetActiveScene().name;
+        int num = int.Parse(currentScene);
+        num += 1;
+        string name = num.ToString();
+        if (BulbsActiveState()) { SceneManager.LoadScene(name); ; }
+
+
+
     }
 }
+
